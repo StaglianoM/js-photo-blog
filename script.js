6d659f04619d260f3160c2dc4832b22b5abefd1e
StaglianoM <stagliano.m92@gmail.com>
2024-11-07 14:41:11 +0100

    add box onclick & display none pin

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById('gallery'); 
-
+    const container = document.getElementById('gallery');
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImage = document.getElementById('lightboxImage');
+    const closeButton = document.getElementById('closeButton');
+  
     function fetchPhotos() {
-        axios
-            .get("https://jsonplaceholder.typicode.com/photos?_limit=6") 
+        axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
             .then((response) => {
                 const photos = response.data;
-                let photoHTML = ""; 
-                photos.forEach((photo) => {
-                    photoHTML +=
+                let photoHTML = "";
+                photos.forEach((photo, index) => {
+                    const pinId = `pin-${index}`;
+                    photoHTML += 
                         `<div class="card">
-                            <img id="pin" src="/img/pin.svg" alt="Pin icon">
-                            <img id="placeholder" src="${photo.thumbnailUrl}" alt="${photo.title}">
+                            <img id="${pinId}" class="pin" src="/img/pin.svg" alt="Pin icon"/>
+                            <img class="thumbnail" src="${photo.thumbnailUrl}" alt="${photo.title}" onclick="showImage('${photo.url}', '${pinId}')">
                             <p>${photo.title}</p>
                         </div>`;
                 });
-                container.innerHTML = photoHTML; 
+                container.innerHTML = photoHTML;
             })
-            .catch((err) => 
-                console.error("Errore generico:", err)
-            );
+            .catch((err) => {
+                console.error("Errore generico:", err);
+            });
     }
 
-    fetchPhotos(); 
+    window.showImage = function(imageUrl, pinId) {
+        lightbox.style.display = "flex";  
+        lightboxImage.src = imageUrl;    
+
+        const pin = document.getElementById(pinId);
+        pin.style.display = "none";  
+        closeButton.style.display = "block";
+    }
+
+    closeButton.onclick = function() {
+        lightbox.style.display = "none";  
+        const pins = document.querySelectorAll('.pin'); 
+        pins.forEach(pin => pin.style.display = "block"); 
+        closeButton.style.display = "none";
+    }
+
+    fetchPhotos();
 });
+
+
